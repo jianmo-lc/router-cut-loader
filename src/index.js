21b3a5852a7ecf8bdc9c0d12b54411f0e394a669
lichao <frank.li@lixinchuxing.com>
2018-10-03 11:00:12 +0800

    logic optimizing

--- src/index.js
+++ src/index.js
@@ -8,20 +8,38 @@ module.exports = function loader(source) {
   const include = options.include
   if (allLoad) return source
   if (!include || (include && !include.length)) {
-    throw new Error('router-cut-loader options require a none empty "include" array if "all" set to true.')
+    throw new Error('router-cut-loader options require a none empty "include" array if "all" set to false.')
   }
   // delete comments
   const noCommentSource = source.replace(/(\/\/.*?[\n\r])|(\/\*[\S\s]*?\*\/)/g, '')
-  // delete router object NOT exists in "include" array in options
-  const reformedSource = noCommentSource.replace(
-    /,*\s*?\{\s*?path\s*?:[\S\s]+?,\s*?component\s*?:[\S\s]+?,\s*?(meta\s*?:\s*?\{[\S\s]*?\})?\s*?\}/g,
-    (str) => {
-      const matched = str.match(/path\s*?:\s+?['"]\/(\S+?)['"\\/]/)
-      if (!matched) return str
-      const pathname = matched[1]
-      if (include.includes(pathname)) return str
-      return ''
+  let noCommentSourceCopy = noCommentSource
+
+  const reg = /[\{\}]/g // eslint-disable-line
+  let match = reg.exec(noCommentSource)
+  let startMatch, endMatch
+  const stack = []
+  while (match) {
+    switch (match[0]) {
+      case '{':
+        stack.push(match)
+        break
+      case '}':
+        startMatch = stack.pop()
+        endMatch = match
+        const subStr = noCommentSource.substring(startMatch.index, endMatch.index + 1)
+        const pathMatch = subStr.match(/path\s*?:[\S\s]+?,/g)
+        const componentMatch = subStr.match(/component\s*?:[\S\s]+?,/g)
+        // make sure that the matched string has and only has one path and one component
+        if ((pathMatch && pathMatch.length === 1) && (componentMatch && componentMatch.length === 1)) {
+          const firstWordOfPath = subStr.match(/path\s*?:\s+?['"]\/(\S+?)['"\\/]/)[1]
+          // delete the router configuration if the first word of the path do not exist in "include" list
+          if (!include.includes(firstWordOfPath)) {
+            noCommentSourceCopy = noCommentSourceCopy.replace(subStr, '')
+          }
+        }
+        break
     }
-  )
-  return reformedSource
+    match = reg.exec(noCommentSource)
+  }
+  return noCommentSourceCopy
 }
